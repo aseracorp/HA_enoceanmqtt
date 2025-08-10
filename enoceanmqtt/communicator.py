29c7937152fab8c9e7fad764157872836e56d31a
Christophe Hardouin Duparc <christophe.hardouin.duparc@gmail.com>
2025-08-10 12:16:09 +0200

    Update communicator.py

--- enoceanmqtt/communicator.py
+++ enoceanmqtt/communicator.py
@@ -297,10 +297,10 @@ class Communicator:
         if profile:
             # Loop over profile contents
             for source in list(profile):
-               if not source.name:
+               if not source.tag:
                    continue
                # Check the current shortcut matches the command shortcut
-               if source['shortcut'] == sensor.get('command'):
+               if source.get('shortcut') == sensor.get('command'):
                    return packet.eep._get_raw(source, packet._bit_data)
 
         # If profile or command shortcut not found,
